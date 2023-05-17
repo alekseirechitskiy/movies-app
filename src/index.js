7db0d66a4e7a0b97015a43648dede15c6dc9f8af
alekseirechitskiy <slowspirit@gmail.com>
2023-05-17 17:23:36 +0300

    Checking husky

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app';
 
-console.log();
+console.log('foo');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
