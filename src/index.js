2ef46151d4f5178f6d5e0d35ab89366fe5ae791f
alekseirechitskiy <slowspirit@gmail.com>
2023-05-17 17:18:10 +0300

    Checking husky

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './components/app';
-var a = 'text';
+
 console.log();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
